2bc66b301a8e87471dfd6dbce136d5d4413166f5
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-02-17 10:32:47 -0800

    update to offiical 4.6.1 build

--- dist/service.worker.js
+++ dist/service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '4.6.beta.custom_build';
+const CACHE_VERSION = '4.6.1.stable.official';
 /** @type {string} */
 const CACHE_PREFIX = 'GodotEngine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
